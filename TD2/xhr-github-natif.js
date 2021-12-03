634741fef966709e548e3785f8475e31cbb5b5a4
Tenebrosful <hugobernard39@gmail.com>
2021-12-03 10:56:35 +0100

    Update xhr-github-natif.js

--- TD2/xhr-github-natif.js
+++ TD2/xhr-github-natif.js
@@ -1,6 +1,38 @@
 // Affiche des informations sur les users Github
 
 /**
+* @typedef {Object} UserData
+* @property {string} login
+* @property {number} id
+* @property {string} node_id 
+* @property {string} avatar_url
+* @property {string} gravatar_id
+* @property {string} url
+* @property {string} html_url
+* @property {string} followers_url
+* @property {string} following_url
+* @property {string} gists_url
+* @property {string} starred_url
+* @property {string} subscriptions_url
+* @property {string} organizations_url
+* @property {string} repos_url
+* @property {string} events_url
+* @property {string} received_events_url
+* @property {string} type
+* @property {string} site_admin
+*/
+
+/**
+* @typedef {Object} DataDepot
+* @property {number} id
+* @property {string} node_id
+* @property {string} name
+* @property {string} full_name
+* @property {boolean} private
+* @property {UserData} owner
+*/
+
+/**
 * envoie un requête XHR
 * @param {string} urlSend URL ou route de l'API
 * @param {function} success Fonction à appeler en cas de succès
@@ -17,7 +49,7 @@ function sendXhr(urlSend, success) {
 
 /**
 * Fonction qui affiche la liste des repos du user
-* @param {Object} reposList Liste des repos du user
+* @param {DataDepot[]} reposList Liste des repos du user
 */
 let displayUserRepos = (reposList) => {
     console.log('liste des repos : ', reposList)
@@ -40,7 +72,12 @@ let getUser = (data, rang) => {
 * @param {int} rang Rang du user
 */
 function getUserRepos(rang) {
-    sendXhr('https://api.github.com/users', function (data) {
+    sendXhr('https://api.github.com/users', 
+    /**
+     * 
+     * @param {UserData[]} data 
+     */
+    function (data) {
         getUser(data, rang)
     })
 }
