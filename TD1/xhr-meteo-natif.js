a145aed1c4f12863bad85cf36fdcd7cc0b04c7b7
Tenebrosful <hugobernard39@gmail.com>
2021-12-03 09:57:57 +0100

    Update xhr-meteo-natif.js

--- TD1/xhr-meteo-natif.js
+++ TD1/xhr-meteo-natif.js
@@ -13,33 +13,33 @@ function sendXhr(urlSend, success) {
 	xhr.open('GET', urlSend);
 	xhr.responseType = 'json';
 	xhr.send();
-	xhr.addEventListener('load', function(reponse) { success(reponse.target.response)});
-	xhr.addEventListener('error', function (reponse) {console.log('data transfert error : ' + reponse)});
+	xhr.addEventListener('load', function (reponse) { success(reponse.target.response) });
+	xhr.addEventListener('error', function (reponse) { console.log('data transfert error : ' + reponse) });
 }
 
 /**
 * Affiche les données météo d'une ville
 * @param {any} data Contient la réponse de l'API au format json
 */
-function showWeatherData (data) {
+function showWeatherData(data) {
 
 	console.log(data);
 	let weatherData = "";
-    weatherData += "<h3>Meteo a " + data.name + "</h3>";
-    weatherData += "<p>Tendance : " + data.weather[0].description;
-    weatherData += " - Temperature : " + data.main.temp + "°C</p>";
-    weatherData += "<p>Pression : " + data.main.pressure + " hPa - Humidite : " + data.main.humidity + "%</p>";
-    document.getElementById('meteo').innerHTML = weatherData;
+	weatherData += "<h3>Meteo a " + data.name + "</h3>";
+	weatherData += "<p>Tendance : " + data.weather[0].description;
+	weatherData += " - Temperature : " + data.main.temp + "°C</p>";
+	weatherData += "<p>Pression : " + data.main.pressure + " hPa - Humidite : " + data.main.humidity + "%</p>";
+	document.getElementById('meteo').innerHTML = weatherData;
 
 }
 
-let apiKey ="45f5f752c0d9d62ff8054e3106fefe6d";
+let apiKey = "45f5f752c0d9d62ff8054e3106fefe6d";
 
 let city = 'Paris, fr';
 
-let options = "&units=metric" + "&lang=fr" 
+let options = "&units=metric" + "&lang=fr"
 
-let weatherUrl1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID="+ apiKey + options;
+let weatherUrl1 = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=" + apiKey + options;
 
 console.log('API URL : ' + weatherUrl1);
 
