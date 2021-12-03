05bbcb7077b4bc89050b6059a37de0471e01d5d4
Tenebrosful <hugobernard39@gmail.com>
2021-12-03 10:37:46 +0100

    Update xhr-github-natif.js

--- TD2/xhr-github-natif.js
+++ TD2/xhr-github-natif.js
@@ -2,8 +2,9 @@
 
 /**
 * envoie un requête XHR
-*  @param {string} urlSend URL ou route de l'API
-*  @param {function} success Fonction à appeler en cas de succès
+* @param {string} urlSend URL ou route de l'API
+* @param {function} success Fonction à appeler en cas de succès
+* @return {Promise<Object>}
 */
 function sendXhr(urlSend, success) {
     let xhr = new XMLHttpRequest();
@@ -14,13 +15,13 @@ function sendXhr(urlSend, success) {
     xhr.addEventListener('error', function (reponse) { console.log('data transfer error' + reponse) });
 }
 
-/** Fonction qui affiche la liste des repos du user
+/**
+* Fonction qui affiche la liste des repos du user
 * @param {Object} reposList Liste des repos du user
 */
 let displayUserRepos = (reposList) => {
     console.log('liste des repos : ', reposList)
 }
-
 
 // pour info : j'utilise la police fira code pour avoir les fleches, et autres codes de dév
 // function getUser (data, uid) { }
@@ -34,8 +35,6 @@ let getUser = (data, userRang) => {
     console.log('user : ', user.login);
     sendXhr(user.repos_url, displayUserRepos);
 }
-
-
 
 /** gere les taches pour afficher les repos d'un user
 * @param {int} user_id Id du user
